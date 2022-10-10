a45f0206b726dc817e7473f5c00c0fb32aa674fa
EndeavoringOrb <aaronbelikoff@gmail.com>
2022-10-10 15:01:30 -0700

    1st commit

--- main.py
+++ main.py
@@ -2,9 +2,17 @@
 
 numbers = [5, 20, 30, 30, 50]
 delval = int(input('Enter the deletion value: '))
+for i in range(len(numbers)):
+    try:
+        numbers.remove(delval)
+        changed = 1
+    except ValueError:
+        if changed == 0:
+            numbers.clear()
+
 
 # ******************************
 # Make your Code
 # ******************************
 
-print (numbers)
+print(numbers)
